a8b350529fb2bec4dec97976dee7bec2b4a1e09c
Andrino Meli <55392123+andrino-meli@users.noreply.github.com>
2024-07-24 14:29:59 +0200

    changes to runime

--- virtual/runtime.h
+++ virtual/runtime.h
@@ -5,6 +5,10 @@
 #define VERBOSE
 #define PRINTF
 
+#define INF (1.0/0.0)
+#define NEGINF (-1.0/0.0)
+#define NAN (0.0/0.0)
+
 extern pthread_barrier_t barr_all;
 
 // ----- Runtime substitutes -----
@@ -23,3 +27,29 @@ inline uint32_t __rt_get_timer();
 
 // ----- Runtime management -----
 int smain(uint32_t coreid);
+
+static inline int is_inf(double x) {
+    return x == INF;
+}
+
+static inline int is_neginf(double x) {
+    return x == NEGINF;
+}
+
+static inline int is_nan(double x) {
+    return x != x;
+}
+
+static inline int is_normal(double x) {
+    return !(is_inf(x) || is_neginf(x) || is_nan(x));
+}
+
+static inline int is_normal_vec(double* x, int len){
+    int isn = 1;
+    for(int i = 0; i < len; i++){
+        if(!is_normal(x[i])){
+            isn = 0;
+        }
+    }
+    return isn;
+}
